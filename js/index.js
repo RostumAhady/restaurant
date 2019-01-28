c3ac00608ee2bb914b9ed638bf504bab8f92897e
Rostum Ahady <rostum_aha@hotmail.co.uk>
2019-01-28 13:59:24 +0000

    menu fade extended

--- js/index.js
+++ js/index.js
@@ -124,7 +124,7 @@ function increaseLineWidth(k, array){
 function removeFade(j, array){
    setTimeout(() => {
     array[j].classList.add('show');
-   }, 180 * j);
+   }, 220 * j);
 }
 
 window.onload = function(){
